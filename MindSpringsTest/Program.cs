fb5b9a7d2d71698c042a908743b11c988f4edc19
kwasikwarteng <megakwasikwarteng@gmail.com>
2024-04-05 11:26:18 +0000

    Service Registrations

--- MindSpringsTest/Program.cs
+++ MindSpringsTest/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IStringTranslatorService, StringTranslatorService>();
 builder.Services.AddScoped<RestExtension>();
 
 //Setup Option Classes
